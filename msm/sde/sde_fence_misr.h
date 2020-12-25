4d7f134b208ab2f6afd0c84d4c7a4f439a61965e
Zhiqiang Liu <zhiqliu@codeaurora.org>
2020-12-25 15:39:03 +0800

    disp: msm: fence: move sub-fence to post-commit fence
    Move sub-fence related structure and operations from sde_fence_file
    to sde_fence_post_commit, and each sub-fence will create its own file
    descriptor.
    
    Add callback functions to post-commit operation structure to make
    sub-fence only visible to internal function inside post-commit. The
    caller of post-commit fence doesn't need to call sub-fence related APIs.
    
    Misr-fence related changes:
    * Move misr signature and config data from misr config queue to misr
      fence, and remove all config queue related code.
    * Move misr fence context from misr structure to sde_crtc, and rename
      to post_commit_fence_ctx. The context will be shared with all
      sub-fence drivers.
    * Move fence force trigger from frame envet to vblank callback.
    
    Change-Id: Iae3b781fa4584aa965d99fe8cc49f821ad2be50a
    Signed-off-by: Zhiqiang Liu <zhiqliu@codeaurora.org>
    Signed-off-by: Zhiqiang Liu <quic_zhiqliu@quicinc.com>

--- msm/sde/sde_fence_misr.h
+++ msm/sde/sde_fence_misr.h
@@ -1,28 +1,18 @@
-/* Copyright (c) 2020, The Linux Foundation. All rights reserved.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 and
- * only version 2 as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (c) 2020-2021, The Linux Foundation. All rights reserved.
  */
 
 #ifndef _SDE_MISR_FENCE_H_
 #define _SDE_MISR_FENCE_H_
 
-#include "sde_fence_post_commit.h"
-#include "sde_fence_file.h"
+#include "sde_crtc.h"
 
 /**
- * sde_misr_fence_create - create a misr fence
- * @fence: the post-commit fence pointer
- *
- * Returns 0 if success, other errno values if failed.
+ * sde_misr_fence_ctx_init - initialize the sub-fence context of misr fence
+ * @sde_crtc: the sde_crtc pointer
  */
-int sde_misr_fence_create(struct sde_post_commit_fence *fence);
+void sde_misr_fence_ctx_init(struct sde_crtc *sde_crtc);
 
 #endif /* _SDE_MISR_FENCE_H_ */
 
