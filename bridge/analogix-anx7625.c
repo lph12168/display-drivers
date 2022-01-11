4b3e10132f7669c04d8b097d6b410445c7692cd2
Zhiqiang Liu <quic_zhiqliu@quicinc.com>
2022-01-11 14:57:59 +0800

    disp: msm: bridge: revert the year modification of copyright
    Revert the year modification of copyright.
    
    Change-Id: I5b77e3d8aab99a86ea37ed8de43a0bd64ae5291c
    Signed-off-by: Zhiqiang Liu <quic_zhiqliu@quicinc.com>

--- bridge/analogix-anx7625.c
+++ bridge/analogix-anx7625.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2018-2021, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2018-2020, The Linux Foundation. All rights reserved.
  *
  * Copyright(c) 2016, Analogix Semiconductor. All rights reserved.
  *
