microsoft/surface-duo-oss-platform.vendor.opensource.display-drivers

disp: msm: dp: add PLUG interrupt support in lphw HPD handler
Currently driver relies on host_deinit to re-enable PLUG detection,
which is controlled from user space. In the case if host_deinit is
late or not called, we'll miss the PLUG interrupt. This change adds
PLUG interrupt support in lphw HPD handler, so HPD can be detected
before user calls to host_deinit.

Change-Id: I8992feb7fa0055e61fce1914095110dc36d80c1d
Signed-off-by: Xiaowen Wu <wxiaowen@codeaurora.org>

msm/dp/dp_lphw_hpd.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (c) 2018-2020, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2018-2021, The Linux Foundation. All rights reserved.
  */
 #define pr_fmt(fmt)	"[drm-dp] %s: " fmt, __func__
 
@@ -189,6 +189,20 @@ static void dp_lphw_hpd_isr(struct dp_hpd *dp_hpd)
 		rc = queue_work(lphw_hpd->connect_wq, &lphw_hpd->attention);
 		if (!rc)
 			pr_debug("attention not queued\n");
+	} else if (isr & DP_HPD_PLUG_INT_STATUS) {
+
+		pr_debug("connect interrupt, hpd isr state: 0x%x\n", isr);
+
+		if (!lphw_hpd->hpd) {
+			lphw_hpd->hpd = true;
+			rc = queue_work(lphw_hpd->connect_wq,
+					&lphw_hpd->connect);
+			if (!rc)
+				pr_debug("connect not queued\n");
+		} else {
+			pr_err("already connected\n");
+		}
+
 	}
 }
 

lph12168/display-drivers

Signed-off-by: Jim Wang <quic_jimwan@quicinc.com>

msm/dp/dp_lphw_hpd.c
@@ -190,6 +190,20 @@ static void dp_lphw_hpd_isr(struct dp_hpd *dp_hpd)
 		rc = queue_work(lphw_hpd->connect_wq, &lphw_hpd->attention);
 		if (!rc)
 			DP_DEBUG("attention not queued\n");
+	} else if (isr & DP_HPD_PLUG_INT_STATUS) {
+
+		DP_DEBUG("connect interrupt, hpd isr state: 0x%x\n", isr);
+
+		if (!lphw_hpd->hpd) {
+			lphw_hpd->hpd = true;
+			rc = queue_work(lphw_hpd->connect_wq,
+					&lphw_hpd->connect);
+			if (!rc)
+				DP_DEBUG("connect not queued\n");
+		} else {
+			DP_ERR("already connected\n");
+		}
+
 	}
 }
 