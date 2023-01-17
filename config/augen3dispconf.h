1ef6bb99c1e750482e7ae3b4218736762104eb44
Ramya SR <quic_rsr@quicinc.com>
2023-01-17 11:31:26 +0530

    msm: disp: Enable SHD & MSM LEASE for augen3
    Enable SHD & LEASE feature for augen3.
    
    Change-Id: I83ca24a9e9bae88e5c2be9fc7ebd3c0402b764d8
    Signed-off-by: Ramya SR <quic_rsr@quicinc.com>

--- config/augen3dispconf.h
+++ config/augen3dispconf.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
- * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #define CONFIG_DRM_MSM 1
@@ -16,4 +16,5 @@
 #define CONFIG_QCOM_MDSS_PLL 1
 #define CONFIG_GKI_DISPLAY 1
 #define CONFIG_MSM_EXT_DISPLAY 0
-#define CONFIG_DRM_SDE_SHD 0
+#define CONFIG_DRM_SDE_SHD 1
+#define CONFIG_DRM_MSM_LEASE 1
