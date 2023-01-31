microsoft/surface-duo-oss-platform.vendor.opensource.display-drivers

disp: msm: sde: add tile swap support in sde encoder
Add tile swap support in sde encoder. If sde connector supports
tile mapping, read connector's tile map and reassign hw_pp to
each tiled phys encoder during mode set.

Change-Id: I92298da6a6cd8bc96c3b6da5288953b78f468584
Signed-off-by: Xiaowen Wu <wxiaowen@codeaurora.org>

msm/sde/sde_encoder.c
@@ -3056,6 +3056,21 @@ static int _sde_encoder_calc_lm_to_intf_ratio(struct sde_encoder_virt *sde_enc,
 	return 0;
 }
 
+static inline void _sde_encoder_get_tile_map(struct sde_encoder_virt *sde_enc,
+		struct drm_connector *connector, int *tile_map)
+{
+	int i, ret;
+
+	ret = sde_connector_get_tile_map(connector,
+			sde_enc->display_num_of_h_tiles, tile_map);
+	if (!ret)
+		return;
+
+	/* default mapping */
+	for (i = 0; i < sde_enc->display_num_of_h_tiles; i++)
+		tile_map[i] = i;
+}
+
 static void sde_encoder_virt_mode_set(struct drm_encoder *drm_enc,
 				      struct drm_display_mode *mode,
 				      struct drm_display_mode *adj_mode)
@@ -3067,6 +3082,7 @@ static void sde_encoder_virt_mode_set(struct drm_encoder *drm_enc,
 	struct drm_connector *conn = NULL, *conn_iter;
 	struct sde_rm_hw_iter dsc_iter, pp_iter, qdss_iter;
 	struct sde_rm_hw_request request_hw;
+	int tile_map[MAX_H_TILES_PER_DISPLAY];
 	bool is_cmd_mode = false;
 	int i = 0, ret;
 	int ratio;
@@ -3195,6 +3211,8 @@ static void sde_encoder_virt_mode_set(struct drm_encoder *drm_enc,
 		return;
 	}
 
+	_sde_encoder_get_tile_map(sde_enc, conn, tile_map);
+
 	for (i = 0; i < sde_enc->num_phys_encs; i++) {
 		struct sde_encoder_phys *phys = sde_enc->phys_encs[i];
 
@@ -3205,7 +3223,7 @@ static void sde_encoder_virt_mode_set(struct drm_encoder *drm_enc,
 				return;
 			}
 
-			phys->hw_pp = sde_enc->hw_pp[i * ratio];
+			phys->hw_pp = sde_enc->hw_pp[tile_map[i] * ratio];
 			phys->connector = conn->state->connector;
 
 			if (phys->ops.mode_set)

lph12168/display-drivers

Signed-off-by: Jim Wang <quic_jimwan@quicinc.com>

msm/sde/sde_encoder.c
@@ -2583,6 +2583,21 @@ static int sde_encoder_virt_modeset_rc(struct drm_encoder *drm_enc,
 	return 0;
 }
 
+static inline void _sde_encoder_get_tile_map(struct sde_encoder_virt *sde_enc,
+		struct drm_connector *connector, int *tile_map)
+{
+	int i, ret;
+
+	ret = sde_connector_get_tile_map(connector,
+			sde_enc->display_num_of_h_tiles, tile_map);
+	if (!ret)
+		return;
+
+	/* default mapping */
+	for (i = 0; i < sde_enc->display_num_of_h_tiles; i++)
+		tile_map[i] = i;
+}
+
 static void sde_encoder_virt_mode_set(struct drm_encoder *drm_enc,
 				      struct drm_display_mode *mode,
 				      struct drm_display_mode *adj_mode)
@@ -2593,6 +2608,7 @@ static void sde_encoder_virt_mode_set(struct drm_encoder *drm_enc,
 	struct sde_connector_state *c_state;
 	struct msm_display_mode *msm_mode;
 	struct sde_crtc *sde_crtc;
+	int tile_map[MAX_H_TILES_PER_DISPLAY];
 	int i = 0, ret;
 	int num_lm, num_intf, num_pp_per_intf;
 
@@ -2671,6 +2687,7 @@ static void sde_encoder_virt_mode_set(struct drm_encoder *drm_enc,
 	if (!num_pp_per_intf)
 		num_pp_per_intf = 1;
 
+	_sde_encoder_get_tile_map(sde_enc, conn, tile_map);
 	/* perform mode_set on phys_encs */
 	for (i = 0; i < sde_enc->num_phys_encs; i++) {
 		struct sde_encoder_phys *phys = sde_enc->phys_encs[i];
@@ -2681,7 +2698,7 @@ static void sde_encoder_virt_mode_set(struct drm_encoder *drm_enc,
 						i, num_pp_per_intf);
 				return;
 			}
-			phys->hw_pp = sde_enc->hw_pp[i * num_pp_per_intf];
+			phys->hw_pp = sde_enc->hw_pp[tile_map[i] * num_pp_per_intf];
 			phys->connector = conn;
 			if (phys->ops.mode_set)
 				phys->ops.mode_set(phys, mode, adj_mode,