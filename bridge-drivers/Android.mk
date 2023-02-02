e998b9db3ff9f0ebdc74d1dad0d08179d69e9c4e
Deven Solanki <quic_dsolanki@quicinc.com>
2023-02-02 20:32:22 +0530

    disp: bridge: fix add missing dependencies for bridge driver
    Change-Id: I7345904a8723cf700bd189fe83bb7ac49cc1a717
    Signed-off-by: Deven Solanki <quic_dsolanki@quicinc.com>

--- bridge-drivers/Android.mk
+++ bridge-drivers/Android.mk
@@ -15,6 +15,12 @@ DLKM_DIR := $(TOP)/device/qcom/common/dlkm
 
 LOCAL_ADDITIONAL_DEPENDENCIES := $(wildcard $(LOCAL_PATH)/**/*) $(wildcard $(LOCAL_PATH)/*)
 
+# List of board platforms for which below driver's API should not be enabled
+NO_MMRM_BOARDS          := kona
+NO_MSM_EXT_DISP_BOARDS  := taro bengal
+NO_SEC_BOARDS           := taro bengal
+NO_HW_FENCE_BOARDS      := taro bengal kona
+
 # Build lt9611uxc.ko
 ###########################################################
 # This is set once per LOCAL_PATH, not per (kernel) module
@@ -22,6 +28,22 @@ KBUILD_OPTIONS := DISPLAY_ROOT=$(DISPLAY_BLD_DIR)
 KBUILD_OPTIONS += MODNAME=lt9611uxc
 KBUILD_OPTIONS += BOARD_PLATFORM=$(TARGET_BOARD_PLATFORM)
 KBUILD_OPTIONS += $(DISPLAY_SELECT)
+
+ifneq ($(TARGET_BOARD_AUTO), true)
+$(info Build the symbols respective to the platform)
+ifneq ($(call is-board-platform-in-list, $(NO_MMRM_BOARDS)),true)
+KBUILD_OPTIONS += KBUILD_EXTRA_SYMBOLS+=$(PWD)/$(call intermediates-dir-for,DLKM,mmrm-module-symvers)/Module.symvers
+endif
+ifneq ($(call is-board-platform-in-list, $(NO_MSM_EXT_DISP_BOARDS)),true)
+KBUILD_OPTIONS += KBUILD_EXTRA_SYMBOLS+=$(PWD)/$(call intermediates-dir-for,DLKM,msm-ext-disp-module-symvers)/Module.symvers
+endif
+ifneq ($(call is-board-platform-in-list, $(NO_SEC_BOARDS)),true)
+KBUILD_OPTIONS += KBUILD_EXTRA_SYMBOLS+=$(PWD)/$(call intermediates-dir-for,DLKM,sec-module-symvers)/Module.symvers
+endif
+ifneq ($(call is-board-platform-in-list, $(NO_HW_FENCE_BOARDS)),true)
+KBUILD_OPTIONS += KBUILD_EXTRA_SYMBOLS+=$(PWD)/$(call intermediates-dir-for,DLKM,hw-fence-module-symvers)/Module.symvers
+endif
+endif
 
 ###########################################################
 include $(CLEAR_VARS)
@@ -31,6 +53,25 @@ LOCAL_MODULE_KBUILD_NAME  := lt9611uxc.ko
 LOCAL_MODULE_TAGS         := optional
 LOCAL_MODULE_DEBUG_ENABLE := true
 LOCAL_MODULE_PATH         := $(KERNEL_MODULES_OUT)
+
+ifneq ($(TARGET_BOARD_AUTO), true)
+ifneq ($(call is-board-platform-in-list, $(NO_MMRM_BOARDS)),true)
+LOCAL_REQUIRED_MODULES    += mmrm-module-symvers
+LOCAL_ADDITIONAL_DEPENDENCIES += $(call intermediates-dir-for,DLKM,mmrm-module-symvers)/Module.symvers
+endif
+ifneq ($(call is-board-platform-in-list, $(NO_MSM_EXT_DISP_BOARDS)),true)
+LOCAL_REQUIRED_MODULES    += msm-ext-disp-module-symvers
+LOCAL_ADDITIONAL_DEPENDENCIES += $(call intermediates-dir-for,DLKM,msm-ext-disp-module-symvers)/Module.symvers
+endif
+ifneq ($(call is-board-platform-in-list, $(NO_SEC_BOARDS)),true)
+LOCAL_REQUIRED_MODULES    += sec-module-symvers
+LOCAL_ADDITIONAL_DEPENDENCIES += $(call intermediates-dir-for,DLKM,sec-module-symvers)/Module.symvers
+endif
+ifneq ($(call is-board-platform-in-list, $(NO_HW_FENCE_BOARDS)),true)
+LOCAL_REQUIRED_MODULES    += hw-fence-module-symvers
+LOCAL_ADDITIONAL_DEPENDENCIES += $(call intermediates-dir-for,DLKM,hw-fence-module-symvers)/Module.symvers
+endif
+endif
 
 include $(DLKM_DIR)/Build_external_kernelmodule.mk
 ###########################################################
