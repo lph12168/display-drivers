694768fc1dbc17dc646b647d5d26e6bf02ad2356
Lei Ma <quic_lem@quicinc.com>
2023-02-20 15:48:06 +0800

    disp: msm-hyp: add loop timeout for message receive
    Add timeout for receive logic to avoid GVM crash.
    
    Change-Id: Ia28d6b048723be0d5b2f95d00da1a04dfe830fc5
    Signed-off-by: Lei Ma <quic_lem@quicinc.com>
    Signed-off-by: Rahul Sharma <quic_rahsha@quicinc.com>

--- msm-hyp/wfd/wire_user.c
+++ msm-hyp/wfd/wire_user.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2017-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <linux/habmm.h>
@@ -904,6 +904,7 @@ wfdDeviceCommitExt_User(
 	struct wire_device *wire_dev = device;
 	struct wire_port *wire_port = hdl;
 	void *handle = wire_dev->ctx->init_info.context;
+	int retry_times = 0;
 
 	/* Request/Response */
 	WIRE_HEAP struct wire_packet req, resp;
@@ -955,6 +956,7 @@ wfdDeviceCommitExt_User(
 	}
 	HYP_ATRACE_END(marker_buff);
 
+retry:
 	/* reset batch commit */
 	if (wire_port->commit.size) {
 		prep_batch_hdr(&wire_port->commit);
@@ -972,7 +974,13 @@ wfdDeviceCommitExt_User(
 		if (user_os_utils_send_recv(handle, (struct wire_packet *)wire_port->commit.packet,
 				&resp, 0x00)) {
 			WIRE_LOG_ERROR("RPC call failed");
-			goto end;
+
+			retry_times++;
+			if (retry_times >= 3) {
+				panic("wfdDeviceCommit");
+			} else {
+				goto retry;
+			}
 		}
 #endif
 		wire_port->commit.size = 0;
